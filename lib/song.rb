606b9127e90857e656b96f635c05a867fdd2a05b
Zannatul Naeem <mollahnaeem@gmail.com>
2020-09-02 03:04:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all 
+  end 
+  
+  def save
+    @@all << self
+  end 
+    
+  
+
+
+
+end
